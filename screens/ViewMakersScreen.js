2a836e62e1b268029be42cc5c70b0a9169ae05c1
pragati basa <pragatibasa@gmail.com>
2019-08-21 15:14:43 +0530

    makers search working.

--- screens/ViewMakersScreen.js
+++ screens/ViewMakersScreen.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {View, Text, ScrollView, StyleSheet, TouchableOpacity, Image, FlatList} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, TouchableOpacity, Image, FlatList, TextInput} from 'react-native';
 import {connect} from "react-redux";
 
 class ViewMakersScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            makers: []
+        };
+    }
+
+    searchFilterFunction = text => {
+        const newData = this.props.makers.filter(item => {
+            const itemData = `${item.hostel.toUpperCase()}   
+            ${item.name.toUpperCase()}`;
+
+            const textData = text.toUpperCase();
+
+            return itemData.indexOf(textData) > -1;
+        });
+
+        this.setState({ makers: newData });
+    };
+
     static navigationOptions = ({ navigation }) => {
         return {
             //Heading/title of the header
@@ -27,14 +47,24 @@ class ViewMakersScreen extends React.Component {
             ),
         };
     };
+
+    componentDidMount() {
+        this.setState({makers: this.props.makers});
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <ScrollView
                     style={styles.container}
                     contentContainerStyle={styles.contentContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Search Makers"
+                        onChangeText={text => this.searchFilterFunction(text)}
+                    />
                     <FlatList
-                        data={this.props.makers}
+                        data={this.state.makers}
                         showsVerticalScrollIndicator={false}
                         numColumns={4}
                         columnWrapperStyle={styles.row}
@@ -49,7 +79,7 @@ class ViewMakersScreen extends React.Component {
                                     }}
                                 />
                                 <Text style={styles.name}>{item.name}</Text>
-                                <Text style={styles.email}>{item.hostel}</Text>
+                                <Text style={styles.hostel}>{item.hostel}</Text>
                             </View>
                         }
                         keyExtractor={item => item.email}
@@ -71,8 +101,41 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
+    contentContainer: {
+        padding: 20,
+        display: 'flex',
+    },
+    input: {
+        height: 40,
+        borderRadius: 9,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 5, height: 12 },
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        elevation: 10,
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 20,
+        paddingLeft: 10,
+        backgroundColor: '#fff',
+        borderWidth: 1
+    },
     row: {
         flex: 1,
         justifyContent: "space-around"
+    },
+    flatview: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10
+    },
+    name: {
+        fontSize: 10
+    },
+    hostel: {
+        fontSize: 10
     }
 });
